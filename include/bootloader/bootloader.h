05315be38a6abe9d4b7b7e46faeb737934a1bfbb
a-int <somemailforicloud@icloud.com>
2023-07-10 21:29:43 +0300

    add simple boot code

--- include/bootloader/bootloader.h
+++ include/bootloader/bootloader.h
@@ -1,1 +1,3 @@
+#include <stm32f411xe.h>
 
+extern uint32_t  _sfirmware[];
---
+++ src/bootloader.c
@@ -0,0 +1,14 @@
+#include <stm32f411xe.h>
+#include <bootloader.h>
+int main(){
+  while (1) {
+    uint32_t* firmware = (uint32_t*)&_sfirmware;
+    __disable_irq();
+    SCB->VTOR = (uint32_t)firmware;
+    __set_MSP(firmware[0]);
+    __enable_irq();
+    void (*Reset_Handler)(void) = (void(*)())(firmware[1]);
+    Reset_Handler();
+  }
+  return 0;
+}
