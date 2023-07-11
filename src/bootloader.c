eaaefe41a7b9f94bf5f1d4ec0dbb70dba6305758
a-int <somemailforicloud@icloud.com>
2023-07-11 18:02:19 +0300

    add sections for __libc_init_array routine

--- src/bootloader.c
+++ src/bootloader.c
@@ -2,7 +2,7 @@
 #include <bootloader.h>
 int main(){
   while (1) {
-    uint32_t* firmware = (uint32_t*)&_sfirmware;
+    uint32_t* firmware = (uint32_t*)_sfirmware;
     __disable_irq();
     SCB->VTOR = (uint32_t)firmware;
     __set_MSP(firmware[0]);
